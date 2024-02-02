a76230f07fd1ed5cf705513b0b3517d0b972b361
sundararajanr <rsundarmail77@gmail.com>
2024-02-02 11:40:19 +0530

    Update tkn.py

--- .github/workflows/tkn.py
+++ .github/workflows/tkn.py
@@ -1,7 +1,1 @@
-code = '"hello" + "world"'
-result = eval(code)
-print(result)  # Output: "hello world"
- 
-code = '["a", "b", "c"][1]'
-result = eval(code)
-print(result)  # Output: "b"
+print('result')  # Output: "b"
